6bac2ae3eaee407fdaa51c9ff4303fbc0eafacef
deepthi-kk <68332814+deepthi-kk@users.noreply.github.com>
2023-04-21 16:09:42 -0700

    Update weather.py

--- weather.py
+++ weather.py
@@ -1,7 +1,7 @@
 import requests
 
 def Weather(city):
-    api_address="http://api.openweathermap.org/data/2.5/weather?q=city&appid=c5ec0648cd942b88dd37affb4dc0ddb9"
+    api_address="http://api.openweathermap.org/data/2.5/weather?q=city&appid=dummy"
     # city = input('Enter the City Name :')
     json_data = requests.get(api_address).json()
     format_add = json_data['main']
