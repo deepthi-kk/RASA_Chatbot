30db9cbe5b3560a92624b2fbfec967911c1666ec
deepthi-kk <68332814+deepthi-kk@users.noreply.github.com>
2023-04-21 16:04:52 -0700

    Update database_connectivity.py

--- database_connectivity.py
+++ database_connectivity.py
@@ -1,7 +1,7 @@
 import mysql.connector
 def DataUpdate(FirstName,LastName,Feedback): 
             mydb = mysql.connector.connect( host="localhost", user="root",  
-            passwd="Prabhavathi_55", database="Rasa") 
+            passwd="dummy", database="Rasa") 
             mycursor = mydb.cursor() 
             #sql = "CREATE TABLE FeedBack_rasa (firstName VARCHAR(255),lastName VARCHAR(255), feedback VARCHAR(255))"
             sql='INSERT INTO FeedBack_rasa_date (firstName, lastName, feedback) VALUES ("{0}","{1}", "{2}");'.format(FirstName,LastName,Feedback) 
